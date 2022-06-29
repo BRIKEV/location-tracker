b40758e3e49c65ef6cdc3e01d3cd26e9f25d248d
kevinccbsg <kevinccbsg@gmail.com>
2022-06-30 01:29:07 +0200

    feat: add rate limiter

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { tagError } = require('error-handler-module');
 const tripsController = require('../controller/trips');
+const limiter = require('../utils/limiter');
 
 const router = express.Router();
 
 const initRouter = ({ store }) => {
   const trips = tripsController({ store });
-  router.post('/trips', async (req, res, next) => {
+  router.post('/trips', limiter, async (req, res, next) => {
     try {
       const trip = await trips.registerTrip(req.body);
       return res.status(201).json({ id: trip.id });
@@ -15,7 +16,7 @@ const initRouter = ({ store }) => {
     }
   });
 
-  router.post('/trips/:id/end', async (req, res, next) => {
+  router.post('/trips/:id/end', limiter, async (req, res, next) => {
     try {
       await trips.endTrip(req.params.id, req.body);
       return res.sendStatus(204);
