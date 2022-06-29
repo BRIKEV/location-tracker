7655807fdd5c02078aa1b6f6f8f8e4c0e68690c0
kevinccbsg <kevinccbsg@gmail.com>
2022-06-30 01:54:35 +0200

    fix: manifest

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    manifest: true,
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:4000',
