0202b0827f6e22f326674917a97516bdbf2c8db1
kevinccbsg <kevinccbsg@gmail.com>
2022-06-30 02:09:39 +0200

    refactor: manifest usage with vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,18 @@ import { defineConfig } from 'vite';
 import i18nResources from 'vite-plugin-i18n-resources';
 import { i18nextScanner } from 'vite-plugin-i18next-scanner';
 import react from '@vitejs/plugin-react';
+import { ViteFaviconsPlugin } from "vite-plugin-favicon";
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    manifest: true,
-  },
   server: {
     proxy: {
       '/api': 'http://localhost:4000',
     },
   },
   plugins: [
+    ...(process.env.BUILD ? [ViteFaviconsPlugin(resolve(__dirname, 'src/img/icons/apple-icon.png'))] : []),
     i18nResources({
       path: resolve(__dirname, 'src/locales'),
     }),
